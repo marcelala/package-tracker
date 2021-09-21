f35b8893a059fff224c2fd850d73456c122b887b
Marcela F <54597344+marcelala@users.noreply.github.com>
2021-09-21 21:23:54 +0200

    Fetch packages from API

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,17 @@
 import Hero from "./Hero";
 import FetchData from "../../utilities/FetchData";
+import PackageCard from "./PackageCard";
+import { useState } from "react";
 
 export default function Home() {
+  const [view, setView] = useState(false);
+  const Cards = FetchData().map((item) => {
+    return <PackageCard parcel={item} key={item.id} />;
+  });
   return (
     <section id="home">
-      <Hero onClick={() => FetchData()} />
+      <Hero onClick={() => setView(true)} />
+      {view && <ul>{Cards}</ul>}
     </section>
   );
 }
