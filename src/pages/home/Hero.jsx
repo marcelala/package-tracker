86abc53e61f47d79263e9af19e3edad7ad5f994d
Marcela F <54597344+marcelala@users.noreply.github.com>
2021-09-22 21:24:11 +0200

    hero styles

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,16 +1,19 @@
 import HeroImg from "../../assets/images/HeroImg.png";
 import Button from "../../components/Button";
 
-export default function Hero({ onClick }) {
-  return (
-    <section id="hero">
-      <h1 className="brand">Busy Bees</h1>
-      <img src={HeroImg} alt="bees carrying cardboard boxes" />
-      <h2>Track your parcels anywhere</h2>
-      <p>Check the progress of all of your deliveries</p>
-      <Button theme={"primary"} onClick={onClick}>
-        Track now
-      </Button>
-    </section>
-  );
+export default function Hero({onClick}) {
+    return (
+        <section id="hero">
+
+            <h1 className="brand">Busy Bees</h1>
+            <img src={HeroImg} alt="bees carrying cardboard boxes"/>
+            <div className="container">
+                <h2>Track your parcels anywhere</h2>
+                <p>Check the progress of all of your deliveries</p>
+            </div>
+            <Button theme={"primary"} onClick={onClick}>
+                Track now
+            </Button>
+        </section>
+    );
 }
